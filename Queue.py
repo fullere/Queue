01595131b285248ea9be62f33b5689715d5069e3
fullere <fullere@hartwick.edu>
2020-03-04 22:42:28 -0500

    wrote code for Queue

--- Queue.py
+++ Queue.py
@@ -1,3 +1,15 @@
 #Elizabeth Fuller
 #3/4/20
 #Queue
+
+from DoubleLinkedListClass.py import Double_Linked_List
+
+class Queue:
+    def __init__(self):
+        self.myList=Double_Linked_List()
+
+    def push(self, data):
+        self.myList.push_end(data)
+
+    def pop(self, data):
+        return self.myList.pop_head(data)
